b52215816fd57216cac82ec6d3b36841e76da95e
Vladimir089 <pickerpikcer@gmail.com>
2024-09-26 17:00:55 +0300

    Initial Commit

---
+++ Betano Estate/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Betano Estate
+//
+//  Created by Владимир Кацап on 26.09.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
